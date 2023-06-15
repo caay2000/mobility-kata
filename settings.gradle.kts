1e82e9f863fa7b57a93bbaa0a2236dd5ecac3936
Albert Casanovas <caay2000@gmail.com>
2023-06-16 01:24:47 +0200

    Initial commit with project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    includeBuild("build-logic")
+}
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "mobility-kata"
+include("app", "list", "utilities")
