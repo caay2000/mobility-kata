6d33a6d1fdfab84ff37b1a0283577cc3fbcab827
Albert Casanovas <caay2000@gmail.com>
2023-06-20 23:54:05 +0200

    Logging and change temurin for adopt

--- libs/dependency-injection/build.gradle.kts
+++ libs/dependency-injection/build.gradle.kts
@@ -8,7 +8,6 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging-jvm")
     implementation("ch.qos.logback:logback-classic")
 
-//    testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core")
 }
