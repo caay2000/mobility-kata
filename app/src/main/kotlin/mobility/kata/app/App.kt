aeaf27c4bf4e9099333227917bd52085dd298822
caay2000 <caay2000@gmail.com>
2023-07-14 02:05:31 +0200

    updated test and random string generator

--- app/src/main/kotlin/mobility/kata/app/App.kt
+++ app/src/main/kotlin/mobility/kata/app/App.kt
@@ -4,7 +4,4 @@
 package mobility.kata.app
 
 fun main() {
-//    val tokens = StringUtils.split(MessageUtils.getMessage())
-//    val result = StringUtils.join(tokens)
-//    println(WordUtils.capitalize(result))
 }
